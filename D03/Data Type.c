76680166d3398f2f77bf4f3ff4a9783098f7f6b1
GIRIDHAR SK <151628450+Giridhar0702@users.noreply.github.com>
2024-05-24 16:17:21 +0530

    Create Data Type.c

---
+++ D03/Data Type.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+  int num = 55;
+  float fnum = 2.55;
+  char letter = 'Y';
+  printf("%d\n", num);
+  printf("%f\n", fnum);
+  printf("%c\n", letter);
+  return 0;
+}
