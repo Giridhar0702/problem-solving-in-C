fe8fa887d4ca98f577a779893289a2de702e6eba
GIRIDHAR SK <151628450+Giridhar0702@users.noreply.github.com>
2024-05-31 18:54:25 +0530

    Update vote eligible.c

--- D09/vote eligible.c
+++ D09/vote eligible.c
@@ -8,7 +8,10 @@ int main()
 
   if (vote_age<18)   
   {
-    printf("You are not eligible to vote.\n");   
+    printf("You are not eligible to vote.\n");  
+  }
     
-  else
+  else{
     printf("You are eligible to vote.\n");  
+  }
+}
